1f54e55d794e6d254736147b132e1bfad658750c
Robert L. Read <read.robert@gmail.com>
2017-08-11 18:10:57 -0500

    adding some data

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 
 var database = {
-    projects: [ project],
-    providers: [ provider ],
-    proposals: [ proposal ]
+    projects: [ projects[0],projects[1]],
+    providers: [ providers[0],providers[1] ],
+    proposals: [ proposals[0], proposals[1] ]
 };
--- datatypes.js
+++ datatypes.js
@@ -1,20 +1,20 @@
+var projects = [];
+var providers = [];
+var proposals = [];
 
-
-var project = { client_id: 0,
+projects[0] = { client_id: 0,
 		title: "Antibiotic resistant bacteria information website",
 		description: "Quickly build a website to link to the latest information on antibiotic resistant bacteria information website",
 		reward_guidance: "only $2,000 for this one",
 		expiration: "08/30/17"
 	      };
 
-
-var provider = { client_id: 0,
+providers[0] = { client_id: 0,
 		name: "Skylight Digital",
 		motto: "We make government work in a digital world"
 	      };
 
-
-var proposal = { client_id: 0,
+proposals[0] = { client_id: 0,
 		 name: " ",
 		 provider_id: 0,
 		 // TODO: This should be done by ids, rather than names
@@ -22,5 +22,32 @@ var proposal = { client_id: 0,
 		 project_name: "Antibiotic resistant bacteria information website",
 		 notes: "We can style it and provide all resources from reputable sites, you will have to confirm you approve each reference",
 		 offer: 2000,
+		 expiration: "08/30/17"
+	      };
+
+
+
+projects[1] = { client_id: 1,
+		title: "Fix SQL Injection Vulnerability",
+		description: "Fix the many documented SQL Injection vulnerabilities in Legacy Codebase #1",
+		reward_guidance: "$3,500",
+		expiration: "08/30/17"
+	      };
+
+
+providers[1] = { client_id: 1,
+		name: "Digitellitechcom",
+		motto: "We use lots of buzzwords."
+	      };
+
+
+proposals[1] = { client_id: 1,
+		 name: " ",
+		 provider_id: 1,
+		 // TODO: This should be done by ids, rather than names
+		 provider_name: "Digitellitechcom",
+		 project_name: "Fix SQL Injection Vulnerability",
+		 notes: "We will fix and make sure the web scan is clear.",
+		 offer: 3500,
 		 expiration: "08/30/17"
 	      };
